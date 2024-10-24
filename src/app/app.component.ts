d18049ffb0be864c2b9d6e8c375501938686a912
sdy329 <sdy329@gmail.com>
2024-10-24 13:09:00 -0400

    Remove unnecessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
--- src/app/modules/addPopup/components/add-popup.component.ts
+++ src/app/modules/addPopup/components/add-popup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
